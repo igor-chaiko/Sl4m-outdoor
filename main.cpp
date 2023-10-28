06342c2b192effe553fc107cc22281b55685e0d8
Rustam Khamidullin <r.khamidullin@g.nsu.ru>
2023-10-28 20:05:00 +0700

    1 version dont work(

--- main.cpp
+++ main.cpp
@@ -1,34 +1,10 @@
 #include <opencv2/opencv.hpp>
+#include "triangulation.h"
+
+
 
 int main() {
-    cv::VideoCapture cap(0);
-
-    cv::Mat frame;
-    cv::Mat grayFrame;
-    std::vector<cv::Point2f> corners;
-
-    while (true) {
-        cap >> frame;
-
-        if (frame.empty()) {
-            break;
-        }
-
-        cv::cvtColor(frame, grayFrame, cv::COLOR_BGR2GRAY);
-
-        cv::goodFeaturesToTrack(grayFrame, corners, 100, 0.05, 15);
-
-        for (const cv::Point2f &corner: corners) {
-            cv::circle(frame, corner, 5, cv::Scalar(0, 0, 255), -1);
-        }
-
-        cv::imshow("Corner Detection", frame);
-
-        cv::waitKey(1);
-    }
-
-    cap.release();
-    cv::destroyAllWindows();
+    triangulation();
 
     return 0;
 }
---
+++ triangulation.h
@@ -0,0 +1,10 @@
+//
+// Created by rustam on 28.10.23.
+//
+
+#ifndef SL4M_OUTDOOR_TRIANGULATION_H
+#define SL4M_OUTDOOR_TRIANGULATION_H
+
+void triangulation();
+
+#endif //SL4M_OUTDOOR_TRIANGULATION_H
