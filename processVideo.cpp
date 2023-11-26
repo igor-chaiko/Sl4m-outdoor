eb66166f647bb5ad2a4c3c51816f437c500affbc
i.chaiko <i.chaiko@g.nsu.ru>
2023-11-26 19:22:18 +0700

    changed entry point

--- processVideo.cpp
+++ processVideo.cpp
@@ -2,37 +2,58 @@
 #include <string>
 #include "processVideo.h"
 
-int entryPoint(const std::string& path) {
-    cv::VideoCapture cap(0);
+std::vector<cv::Mat> init() {
+    std::vector<cv::Mat> frames;
+    frames.reserve(NUMBER_OF_FRAMES);
+
+    for (int i = 0; i < NUMBER_OF_FRAMES; i++) {
+        frames.emplace_back();
+    }
+
+    return frames;
+}
+
+int getFramesPull(std::vector<cv::Mat> &frames, cv::VideoCapture cap) {
     cv::Mat frame;
+
+    for (int i = 0; i < NUMBER_OF_FRAMES; i++) {
+        cap >> frame;
+
+        if (frame.empty() && i == 0) {
+            std::cerr << "Нет кадра( " << std::endl;
+            return -1;
+        }
+
+        frames[i] = frame;
+    }
+
+    return 0;
+}
+
+int entryPoint(const std::string &path) {
+    cv::VideoCapture cap(0);
 
     if (!cap.isOpened()) {
         std::cerr << "Ошибка при открытии видео!" << std::endl;
         return -1;
     }
 
-    // в векторе хранятся 2 окна
-    std::vector<cv::Mat> previousFrames;
+    std::vector<cv::Mat> firstWindow = init(), secondWindow = init();
+
+    int status = getFramesPull(firstWindow, cap);
+    status = getFramesPull(secondWindow, cap);
+
+    if (firstWindow.empty() || secondWindow.empty()) {
+        return -1;
+    }
 
     while (true) {
-        if (previousFrames.size() == NUMBER_OF_FRAMES * 2) {
-            //...
-        }
 
-        cap >> frame;
+        //...
 
-        if (frame.empty()) {
-            std::cout << "Конец видео" << std::endl;
-            break;
-        }
-
-        previousFrames.push_back(frame);
-
-        if (previousFrames.size() > NUMBER_OF_FRAMES * 2) {
-            previousFrames.erase(previousFrames.begin());
-        }
-
-        cv::imshow("Видосик", frame);
+        firstWindow = secondWindow;
+        status = getFramesPull(secondWindow, cap);
+        //...
 
         if (cv::waitKey(30) == 27) {
             break;
--- processVideo.h
+++ processVideo.h
@@ -1,6 +1,6 @@
 #ifndef SL4M_OUTDOOR_PROCESSVIDEO_H
 #define SL4M_OUTDOOR_PROCESSVIDEO_H
-#define NUMBER_OF_FRAMES 5
+#define NUMBER_OF_FRAMES 10
 
 #include <iostream>
 
