359015c6cb0276e03dea94b6af82bf186a44b319
Ruslan Chudinov <r.chudinov@g.nsu.ru>
2024-04-01 04:33:04 +0700

    Добавленная функция тестирования + основная часть результата тестов

---
+++ headers/hashing.h
@@ -0,0 +1,18 @@
+//
+// Created by Ruslan Chudinov on 01.04.2024.
+//
+
+#ifndef SL4M_OUTDOOR_HASHING_H
+#define SL4M_OUTDOOR_HASHING_H
+
+#include <opencv2/opencv.hpp>
+#include <opencv2/img_hash.hpp>
+
+class Hashing {
+private:
+    static double test_one(const cv::Mat &a, const cv::Mat &b);
+public:
+    void test1(const std::string& pathToVideoFile, long frameIndex);
+};
+
+#endif //SL4M_OUTDOOR_HASHING_H
