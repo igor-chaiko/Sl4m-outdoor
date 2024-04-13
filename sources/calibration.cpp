c36461f99c25b4a1b712d54bd7d7efc0d9da35d5
Rustam Khamidullin <r.khamidullin@g.nsu.ru>
2024-04-14 00:32:02 +0700

    calibrate new camera

--- sources/calibration.cpp
+++ sources/calibration.cpp
@@ -5,7 +5,7 @@
 #include <fstream>
 
 #define BOARD_WIDTH 5
-#define BOARD_HEIGHT 7
+#define BOARD_HEIGHT 5
 
 int calibration() {
     //Координаты квадратиков на шахматной доске в системе координат самой шахаматной доски(z == 0)
@@ -23,7 +23,7 @@ int calibration() {
     //Вектор названий изображения
     std::vector<cv::String> images;
     //Получаем пути изображений по которым будем калиброваться
-    cv::glob("../CalibImg/*.jpg", images);
+    cv::glob("../resources/chessboard/camera2/*.jpeg", images);
 
     //Матрица для хранения изображения в серых тонах
     cv::Mat gray;
@@ -31,69 +31,37 @@ int calibration() {
     //Критерий остановки. Максимальное количество итераций - 30, точность - 0.001
     cv::TermCriteria criteria = cv::TermCriteria(cv::TermCriteria::EPS + cv::TermCriteria::MAX_ITER, 30, 0.001);
 
-    //Цикл по всем путям
+// Цикл по всем путям
     for (const auto &image: images) {
-
-        //Считываем изображение
+        // Считываем изображение
         cv::Mat img = cv::imread(image);
         if (img.empty()) {
             continue;
         }
 
-        //преобразовываем в серое изображение
+        // Преобразовываем в серое изображение
         cv::cvtColor(img, gray, cv::COLOR_BGR2GRAY);
 
-        //Координаты углов
+        // Координаты углов
         std::vector<cv::Point2f> corners;
 
-        //Выполняется поиск углов на шахматной доске.
+        // Выполняется поиск углов на шахматной доске.
         bool ret = cv::findChessboardCorners(gray, cv::Size(BOARD_HEIGHT, BOARD_WIDTH), corners,
                                              cv::CALIB_CB_ADAPTIVE_THRESH | cv::CALIB_CB_FAST_CHECK);
-        //Если нашли, то запоминаем результат
+
+        // Если нашли, то рисуем сетку на изображении
         if (ret) {
-            objPoints.push_back(objP);
-            //Метод увеличивает точность найденных координат углов
+            // Метод увеличивает точность найденных координат углов
             cv::cornerSubPix(gray, corners, cv::Size(11, 11), cv::Size(-1, -1), criteria);
-            imgPoints.push_back(corners);
+
+            // Рисуем сетку
+            cv::drawChessboardCorners(img, cv::Size(BOARD_HEIGHT, BOARD_WIDTH), corners, ret);
+
+            // Показываем изображение с нарисованной сеткой
+            cv::imshow("Chessboard Corners", img);
+            cv::waitKey(0); // Ждем нажатия клавиши, чтобы закрыть окно
         }
-    }
-
-    //Матрица камеры, Коэфициенты дисторсии
-    cv::Mat cameraMatrix, distCoefficients;
-    //Векторы вращения  трансляции. В алгоритме не используются(заглушки)
-    std::vector<cv::Mat> rvecs, tvecs;
-
-    //Функция ради которого всё затевалось
-    cv::calibrateCamera(objPoints, imgPoints, gray.size(), cameraMatrix, distCoefficients, rvecs, tvecs);
-
-    //Вывод camera matrix и матрицы коэффициентов дисторсии
-    std::cout << "Camera Matrix:\n" << cameraMatrix << "\n\n";
-    std::cout << "Distortion Coefficients:\n" << distCoefficients << "\n";
-
-    //Открытие файла на запись
-    std::ofstream calib("CalibratedCamera.txt");
-    if (calib.is_open()) {
-        //Записываем Матрицу
-        calib << "Camera Matrix: ";
-        for (int i = 0; i < cameraMatrix.rows; ++i) {
-            for (int j = 0; j < cameraMatrix.cols; ++j) {
-                calib << cameraMatrix.at<double>(i, j) << " ";
-            }
-        }
-        calib << "\n";
-        calib << "Distorsia coefficients: ";
-        for (int i = 0; i < distCoefficients.rows; ++i) {
-            for (int j = 0; j < distCoefficients.cols; ++j) {
-                calib << distCoefficients.at<double>(i, j) << " ";
-            }
-        }
-        calib << "\n";
-        calib.close();
-    } else {
-        std::cerr << "Unable to open CalibratedCamera.txt for writing.\n";
-        return -1;
     }
 
     return 0;
 }
-
