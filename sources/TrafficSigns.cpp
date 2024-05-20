55adde1c96798f0c76adfbab73f3a6c9c9b8de70
i.chaiko <i.chaiko@g.nsu.ru>
2024-05-20 20:06:27 +0700

    ready to merge

---
+++ sources/TrafficSigns.cpp
@@ -0,0 +1,112 @@
+#include "../headers/TrafficSigns.h"
+#include <opencv2/img_hash.hpp>
+
+std::vector<cv::Mat> TrafficSigns::findSigns(const cv::Mat &image) {
+    std::vector<cv::Mat> res;
+    cv::Mat image_hsv, dilateMask;
+    std::vector<cv::Mat> masks;
+
+    // переход в hsv
+    cv::cvtColor(image, image_hsv, cv::COLOR_BGR2HSV);
+
+    cv::Scalar lower_red(0, 10, 10);
+    cv::Scalar upper_red(10, 255, 255);
+    cv::Mat mask1, mask2;
+    cv::inRange(image_hsv, lower_red, upper_red, mask1);
+    cv::Scalar lower_red2(170, 10, 10);
+    cv::Scalar upper_red2(180, 255, 255);
+    cv::inRange(image_hsv, lower_red2, upper_red2, mask2);
+    cv::Mat red_mask;
+    cv::addWeighted(mask1, 1.0, mask2, 1.0, 0.0, red_mask);
+    masks.push_back(red_mask);
+
+    cv::Scalar lower_yellow(35, 70, 50);
+    cv::Scalar upper_yellow(40, 255, 255);
+    cv::Mat yellow_mask;
+    cv::inRange(image_hsv, lower_yellow, upper_yellow, yellow_mask);
+    masks.push_back(yellow_mask);
+
+    cv::Scalar lower_blue(100, 70, 50);
+    cv::Scalar upper_blue(120, 255, 255);
+    cv::Mat blue_mask;
+    cv::inRange(image_hsv, lower_blue, upper_blue, blue_mask);
+    masks.push_back(blue_mask);
+
+    for (const cv::Mat& mask : masks) {
+        // "расширение" маски - нужно для заполнения разрывов, не факт, что нам нужно
+        cv::dilate(mask, dilateMask, cv::Mat(), cv::Point(-1, -1), 2);
+
+        // извлекаем нужные контуры
+        std::vector<std::vector<cv::Point>> contours;
+        std::vector<cv::Vec4i> hierarchy;
+        cv::findContours(dilateMask.clone(), contours, hierarchy, cv::RETR_EXTERNAL, cv::CHAIN_APPROX_SIMPLE);
+
+        for (const auto & contour : contours) {
+            double area = cv::contourArea(contour);
+            if (AREA_LOWER_BOUND < area && area < AREA_UPPER_BOUND) {
+                std::vector<cv::Point> approx;
+                cv::approxPolyDP(contour, approx, 0.03 * cv::arcLength(contour, true), true);
+
+                double perimeter = cv::arcLength(contour, true);
+                double r = perimeter / (2 * CV_PI);
+                double circularity = (area*2)/(perimeter*r);
+
+                double a = sqrt(area);
+                double k1 = (4 * area) / (perimeter * a);
+
+                double b = perimeter / 3;
+                double k2 = ( b * b * sqrt(3) / 4) / area;
+
+                if ((k1 > 0.8 && k1 < 1.2) || (k2 > 0.8 && k2 < 1.2)
+                    || (circularity > 0.75 && circularity < 1.2)) {
+
+                    cv::Rect bounding_rect = cv::boundingRect(contour);
+
+                    cv::Mat contour_image = image(bounding_rect).clone();
+                    res.push_back(contour_image);
+                }
+            }
+        }
+    }
+
+    return res;
+}
+
+std::vector<std::pair<cv::Mat, std::string>> TrafficSigns::processSigns(const cv::Mat& image) {
+    bool newSign;
+    double currentSimilarity;
+    cv::Ptr<cv::img_hash::ImgHashBase> hashFunc;
+    hashFunc = cv::img_hash::PHash::create();
+    TrafficSigns::frameNum++;
+    std::vector<cv::Mat> subTotal = TrafficSigns::findSigns(image);
+    std::vector<std::pair<cv::Mat, std::string>> result;
+
+    for (cv::Mat& sign : subTotal) {
+        newSign = true;
+        cv::Mat currHash;
+        hashFunc->compute(sign, currHash);
+
+        for (const std::pair<int, cv::Mat>& pair : TrafficSigns::metSigns) {
+            int frameNumInPair = pair.first;
+            currentSimilarity = hashFunc->compare(pair.second, currHash);
+            if (currentSimilarity < SIMILARITY && frameNum - frameNumInPair < DISTANCE_BETWEEN_TWO) {
+                newSign = false;
+                break;
+            }
+        }
+
+        if (newSign) {
+            cv::Rect bounding_rect = cv::boundingRect(sign);
+            std::string location;
+            if (bounding_rect.x < (sign.cols / 2)) {
+                location = "left";
+            } else {
+                location = "right";
+            }
+
+            result.emplace_back(sign, location);
+        }
+    }
+
+    return result;
+}
--- sources/main.cpp
+++ sources/main.cpp
@@ -1,118 +1,21 @@
 #include <opencv2/opencv.hpp>
-#include "../headers/trafficSignsDetection.h"
-#include <opencv2/img_hash.hpp>
-#include "../headers/triangulation.h"
+#include "../headers/processVideo.h"
+#include "../headers/Map.h"
 
 int main() {
-    cv::VideoCapture cap("test7.MOV");
-    cv::Ptr<cv::img_hash::ImgHashBase> hashFunc;
-    hashFunc = cv::img_hash::PHash::create();
-    std::vector<cv::Mat> total;
-    std::vector<cv::Mat> hashes;
-    std::vector<std::pair<int, cv::Mat>> hashes2;
-    double similarity;
-    bool differentFrame;
-    int frameNum = 0, z = 0;
-    cv::Mat image, hsv, ex1 = cv::imread("ex3.jpg"); // 1, 2, 1, 2, 4, 2, 1, 2, 1) / 16.0
-    cv::Mat kernel = (cv::Mat_<double>(3, 3) << -1, -1, -1, -1, 8, -1, -1, -1, -1) / 1.0;
 
-    while (true) {
-        frameNum++;
-        cap >> image;
 
-        if (image.empty()) {
-//            for (int i = 0; i < total.size(); i++) {
-//                cv::imshow("image" + std::to_string(i+1), total[i]);
-//            }
-
-            if (cv::waitKey(0) == 27) {
-                break;
-            }
-        }
-
-//        cv::imshow("image", image);
-
-        std::vector<cv::Mat> res = findSigns(image);
-
-        for (cv::Mat& currSign : res) {
-            cv::Mat currHash;
-            hashFunc->compute(currSign, currHash);
-            differentFrame = true;
-
-            std::cout << hashes2.size() << ": ";
-            for (const std::pair<int, cv::Mat>& pair : hashes2) {
-                int frameNumInPair = pair.first;
-                similarity = hashFunc->compare(pair.second, currHash);
-                std::cout << similarity << " ";
-                if (similarity < 26 && frameNum - frameNumInPair < 210) {
-                    differentFrame = false;
-                    break;
-                }
-            }
-            std::cout << std::endl;
-
-            if (differentFrame) {
-                cv::Mat resizedSample;
-                total.push_back(currSign);
-
-                cv::resize(ex1, resizedSample, currSign.size());
-
-                std::pair<int, cv::Mat> pair(frameNum, currHash);
-                cv::imshow("contour_" + std::to_string(++z), currSign);
-                hashes2.push_back(pair);
-                cv::cvtColor(currSign, currSign, cv::COLOR_BGR2GRAY);
-                cv::cvtColor(resizedSample, resizedSample, cv::COLOR_BGR2GRAY);
-
-                cv::Mat dst1, dst2;
-                cv::filter2D(currSign, dst1, -1, kernel);
-                cv::imshow("currSign", dst1);
-//                std::cout <<"1: " << dst1 << std::endl;
-//                currSign = convolution(currSign, kernel);
-                cv::filter2D(resizedSample, dst2, -1, kernel);
-//                std::cout <<"2: " << dst2 << std::endl;
-                cv::imshow("resizedSample", dst2);
-//                resizedSample = convolution(resizedSample, kernel);
-
-                double dist = euclideanDistance(dst1, dst2);
-
-                std::cout << "db2: " << dist << std::endl;
-
-                if (cv::waitKey(0) == 27) {
-
-                }
-            }
-        }
-
-//        cv::imshow("video", image);
-        cv::waitKey(1);
-        cv::destroyWindow("image");
+    try {
+        startProcessing();
+    } catch (const std::runtime_error &e) {
+        std::cerr << e.what() << std::endl;
     }
 
-//    Hashing hashing = Hashing();
-//    try {
-//        hashing.test1("../resources/testVideo2.MOV", 200);
-//    } catch (const std::runtime_error& e) {
-//        std::cerr << e.what() << std::endl;
-//    }
 
-//    try {
-//        startProcessing();
-//    } catch (const std::runtime_error& e) {
-//        std::cerr << e.what() << std::endl;
-//    }
-//
-//
-//
-//    // Ваша исходная матрица
-//    cv::Mat matrix = (cv::Mat_<double>(3, 10) <<
-//                                              1.0, 2.0, -3.0, 4.0, 5.0, 6.0, -7.0, 8.0, 9.0, 0.0,
-//            -1.0, -9.0, -13.0, -14.0, -15.0, -16.0, -17.0, -18.0, -19.0, 9.999,
-//            2.0, 9.999, -23.0, 24.0, 25.0, 26.0, -27.0, 28.0, 29.0, 9.0);
-//
-//
-//    std::cout << "Матрица после удаления колонок:\n" << matrix << std::endl;
-//
-//    std::cout << "Матрица после удаления колонок:\n" << matrix << std::endl;
+
+
+
+
 
     return 0;
 }
